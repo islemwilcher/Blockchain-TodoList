e3747b94e3047e5f0e23558a0cce1d659a5c7ece
islemwilcher <islemwilcher8@gmail.com>
2023-02-03 16:13:30 +0100

    first commit initialize a blockchain dapp

---
+++ contracts/TodoList.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.4.16 <0.9.0;
+
+contract TodoList {
+  // state variable 
+  // represent the state of the smart contract on the blockchain
+  uint public taskCount = 0;
+
+}
